9afdd1ec99a2821fcaa5aa953548e33206d3c574
yuicer <yuicer@yeah.net>
2017-07-10 11:22:33 +0800

    1

--- source/album/main.js
+++ source/album/main.js
@@ -9,20 +9,18 @@ var album_box = new Vue({
 	},
 	methods: {
 		get_img() {
-			var req = new XMLHttpRequest();
+			var req = new XMLHttpRequest(),
+				me = this;
 			req.onreadystatechange = function () {
 				if (req.readyState == 4 && req.status == 200) {
-					//处理取得的歌词的要放这里面
 					var imgsrc1 = req.responseText.split(",");
 					for (var i = 0, l = imgsrc1.length; i < l; i++) {
-						imgsrc_[i] = "http://olti9qjwg.bkt.clouddn.com" + imgsrc1[i].slice(1);
+						me.imgsrc.unshift("http://olti9qjwg.bkt.clouddn.com" + imgsrc1[i].slice(1));
 					}
-					album_box.$data.imgsrc = imgsrc_;
 				}
 			}
 			req.open("GET", "./imgsrc.txt", true);
 			req.send();
 		}
 	}
-
 });
