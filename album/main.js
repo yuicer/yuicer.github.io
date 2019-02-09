ad7fae95fbb88275c8a43d9c3fb0c68f6086a8aa
yuicer <yuicer@yeah.net>
2019-02-09 02:49:57 +0000

    Deploy yuicer/yuicer.github.io to github.com/yuicer/yuicer.github.io.git:master

--- album/main.js
+++ album/main.js
@@ -1,26 +1,26 @@
-var imgsrc_ = [];
+var imgsrc_ = []
 var album_box = new Vue({
-  el: "#album_box",
+  el: '#album_box',
   data: {
-    imgsrc: [],
+    imgsrc: []
   },
-  mounted: function () {
-    this.get_img();
+  mounted: function() {
+    this.get_img()
   },
   methods: {
     get_img() {
       var req = new XMLHttpRequest(),
-        me = this;
-      req.onreadystatechange = function () {
+        me = this
+      req.onreadystatechange = function() {
         if (req.readyState == 4 && req.status == 200) {
-          var imgsrc1 = req.responseText.split(",");
+          var imgsrc1 = req.responseText.split(',')
           for (var i = 0, l = imgsrc1.length; i < l; i++) {
-            me.imgsrc.unshift("http://olti9qjwg.bkt.clouddn.com" + imgsrc1[i].slice(1) + "-reduce");
+            me.imgsrc.unshift('https://yuicer.com/images/' + imgsrc1[i])
           }
         }
       }
-      req.open("GET", "./imgsrc.txt", true);
-      req.send();
+      req.open('GET', './imgsrc.txt', true)
+      req.send()
     }
   }
-});
+})
